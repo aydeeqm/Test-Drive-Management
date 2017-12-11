7f02fd903b1a8c6005c5bd80c35dd96f02cc2a0a
claudia <Clauloayza>
2017-12-11 18:56:17 -0500

    imagenes

--- src/data.js
+++ src/data.js
@@ -1,6 +1,10 @@
+
+
+
 export const cars = [
     {
         marca: susuki,
+        img: 'img',
         modelo: [
             {
                 id: DP01,
@@ -9,6 +13,7 @@ export const cars = [
                 fabricación: '2017',
                 serie:'TSMYE21SXJM346793',
                 placa: 'AXZ444',
+                
             },
             {
                 id: DP02,
@@ -21,7 +26,154 @@ export const cars = [
         ]
     },
     {
-        marca: asdasdas
-    }
+        marca: mazda,
+        modelo: [
+            {
+                id: MD01,
+                modelo: 'Mazda CX5 MT 2.0 2WD 65 Core new',
+                color: ['Gris metálico','Blanco'],
+                fabricación: '2017',
+                serie:'MTCX31ASDS324456',
+                placa: 'MTX435',
+            },
+            {
+                id: MD02,
+                modelo: 'Mazda MX-5 At 2.0 65 High RF',
+                color: ['Negro', 'Rojo'],
+                fabricación: '2017',
+                serie:'MTCX56ASDF564345',
+                placa: 'TMX456',
+            }
+        ]
+    },
+    {
+        marca: citroen,
+        modelo: [
+            {
+                id: CT01,
+                modelo: 'Citroen C4 1.6 VTI 120HP BVMS FEEL',
+                color: ['blanco','negro'],
+                fabricación: '2017',
+                serie:'CTRN23GHJV453422',
+                placa: 'CTR346',
+            },
+            {
+                id: CT02,
+                modelo: 'Citroen C4 Cactus 1.2 pure tech 82 BVM Feel',
+                color: ['negro','gold'],
+                fabricación: '2017',
+                serie:'CTRP45sdfg462367',
+                placa: 'CTS456',
+            }
+        ]
+    },
+    {
+        marca: DsAutomobiles,
+        modelo: [
+            {
+                id: DS01,
+                modelo: 'DS4 Crossback',
+                color: ['Pearl White', 'Ink Blue'],
+                fabricación: '2017',
+                serie:'DSAU45G34367',
+                placa: 'DSA456',
+            },
+            {
+                id: DS02,
+                modelo: 'New DS5',
+                color: ['Ink Blue','latinium Grey'],
+                fabricación: '2017',
+                serie:'DSAU45G4535',
+                placa: 'ASD564',
+            }
+        ]
+    },
+    {
+        marca: Haval,
+        modelo: [
+            {
+                id: HV01,
+                modelo: 'Haval H2 MTR GW4G15B',
+                color: ['Negro','Marfil'],
+                fabricación: '2016',
+                serie:'HVHT45SDKK4656',
+                placa: 'PPT456',
+            },
+            {
+                id: HV02,
+                modelo: '',
+                color: [],
+                fabricación:'',
+                serie:'',
+                placa: '',
+            }
+        ]
+    },
+    {
+        marca: GreatWall,
+        modelo: [
+            {
+                id: DS01,
+                modelo: '',
+                color: [],
+                fabricación: '',
+                serie:'',
+                placa: '',
+            },
+            {
+                id: DS02,
+                modelo: '',
+                color: [],
+                fabricación:'',
+                serie:'',
+                placa: '',
+            }
+        ]
+    },
+    {
+        marca: Changam,
+        modelo: [
+            {
+                id: DS01,
+                modelo: '',
+                color: [],
+                fabricación: '',
+                serie:'',
+                placa: '',
+            },
+            {
+                id: DS02,
+                modelo: '',
+                color: [],
+                fabricación:'',
+                serie:'',
+                placa: '',
+            }
+        ]
+    },
+    {
+        marca: JAC,
+        modelo: [
+            {
+                id: DS01,
+                modelo: '',
+                color: [],
+                fabricación: '',
+                serie:'',
+                placa: '',
+            },
+            {
+                id: DS02,
+                modelo: '',
+                color: [],
+                fabricación:'',
+                serie:'',
+                placa: '',
+            }
+        ]
+    },
 ]
+
+        
+        
 

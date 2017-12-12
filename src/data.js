2ec1fd8e13e2fb00c4c3e1f06e8c92707ed83ab5
claudia <Clauloayza>
2017-12-11 22:41:01 -0500

    import

--- src/data.js
+++ src/data.js
@@ -1,7 +1,29 @@
+import susuki1 from '../imgs/susuki/newvitara.png';
+import susuki2 from '../imgs/susuki/nomade.png';
+import susuki3 from '../imgs/susuki/s-cross.png';
+import mazda1 from '../imgs/mazda/mazda3sport.png';
+import mazda2 from '../imgs/mazda/mazdacx5.png';
+import mazda3 from '../imgs/mazda/mazdamx5rf.png';
+import citroen1 from '../imgs/citroen/citroen1.png';
+import citroen2 from '../imgs/citroen/citroencactus2.png';
+import citroen3 from '../imgs/citroen/citroEnlysse.png';
+import DsAutomobiles1 from '../imgs/ds/ds4crossback.png';
+import DsAutomobiles2 from '../imgs/ds/newds3.png';
+import DsAutomobiles2 from '../imgs/ds/newds5.png';
+import haval1 from '../imgs/haval/havalh2black.png';
+import haval2 from '../imgs/haval/havalh6sport.png';
+import greadwall1 from '../imgs/greatwall/newvoleex.png';
+import greadwall2 from '../imgs/greatwall/wingle5.png';
+import changan1 from '../imgs/changan/changancs15.png';
+import changan2 from '../imgs/changan/changancx70.png';
+import jac1 from '../imgs/jac/grands3.png';
+import jac2 from '../imgs/jac/j4.png';
+
+
 export const cars = [
     {
         marca: 'susuki',
-        img: 'img',
+        imgs: 'imgs',
         modelo: [
             {
                 id: 'DP01',
